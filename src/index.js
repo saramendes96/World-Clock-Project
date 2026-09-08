cb909b5313bf64b3df1708f4def2d6fcf63da6c5
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-08 01:31:47 +0100

    Add date and time elements for Los Angeles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector (".date");
+let losAngelesTimeElement = losAngelesElement.querySelector (".time");
+let losAngelesTime = moment();
+
+losAngelesDateElement.innerHTML = moment().format ("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = 
