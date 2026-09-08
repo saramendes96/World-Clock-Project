62a11197141419b5384bde4d01366a03406bdec1
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-08 14:28:35 +0100

    Fix Los Angeles time display with timezone
    Updated Los Angeles time to use timezone and added duplicate code for displaying date and time.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector (".date");
 let losAngelesTimeElement = losAngelesElement.querySelector (".time");
-let losAngelesTime = moment();
+let losAngelesTime = moment().tz("America/Los Angeles");
+
+losAngelesDateElement.innerHTML = losAngelesTime.format ("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format ("h:mm:ss [<small>]A [<small>]");
+
+
+let parisElement = document.querySelector("#paris");
+let losAngelesDateElement = losAngelesElement.querySelector (".date");
+let losAngelesTimeElement = losAngelesElement.querySelector (".time");
+let losAngelesTime = moment().tz("America/Los Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format ("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format ("h:mm:ss [<small>]A [<small>]");
