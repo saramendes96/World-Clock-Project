47c696ff53566c5aee07351dc9d549f5b9c7b880
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-09 19:35:38 +0100

    Add cities select element to index.js

--- src/index.js
+++ src/index.js
@@ -20,3 +20,5 @@ parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
