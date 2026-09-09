860a675139a081373d9dfec5b39980866c6e54fb
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-09 14:35:14 +0100

    Fix timezone string for Los Angeles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function updateTime() {
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector (".date");
 let losAngelesTimeElement = losAngelesElement.querySelector (".time");
-let losAngelesTime = moment().tz("America/Los Angeles");
+let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format ("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = losAngelesTime.format ("h:mm:ss [<small>]A [<small>]");
