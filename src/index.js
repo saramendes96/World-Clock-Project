c59cb6fc2cc6ec4700e4610198270f1d21069920
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-11 15:38:09 +0100

    Call updateTime function immediately on load

--- src/index.js
+++ src/index.js
@@ -32,8 +32,7 @@ function updateCity(event) {
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
   </div>`;
-
-  updateTime();
 }
 
+updateTime();
 setInterval(updateTime, 1000);
