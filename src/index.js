1523ff37aed37c00acb5fd10ce679c39a874db56
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-11 15:24:09 +0100

    Fix HTML structure for city element rendering

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
 
-  citiesElement.innerHTML = `<div class="city">
+  citiesElement.innerHTML = `<div class="city" data-timezone="${cityTimeZone}">
     <h2>${cityName}</h2>
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
