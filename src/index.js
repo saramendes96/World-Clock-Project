eed50a966aeca6f2174a580e8289fb1f0a14c4e5
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-11 09:03:48 +0100

    Select cities element in updateCity function

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ function updateCity(event) {
 let cityTimeZone = event.target.value;
 let cityName = cityTimeZone.replace ("_", "").split("/")[1];
 let cityTime = moment().tz(cityTimeZone);
+let citiesElement = document.querySelector("#cities");
 }
 
 
