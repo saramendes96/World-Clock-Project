6e7ff8e05d84d19b769e16a28cc65b7bada5194a
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-11 09:40:57 +0100

    Update citiesElement with city information
    Update the inner HTML of citiesElement to display city name, date, and time.

--- src/index.js
+++ src/index.js
@@ -23,6 +23,12 @@ let cityTimeZone = event.target.value;
 let cityName = cityTimeZone.replace ("_", "").split("/")[1];
 let cityTime = moment().tz(cityTimeZone);
 let citiesElement = document.querySelector("#cities");
+citiesElement.innerHTML = `<div class="city">
+        <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
+      </div>
+    </div>
 }
 
 
