bf3b7aeca9d5dbf6d6a80fe561112dd5c3ec06e0
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-11 09:47:49 +0100

    Fix HTML closing tag in index.js

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ citiesElement.innerHTML = `<div class="city">
       <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
       <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
       </div>
-    </div>
+    </div>`;
 }
 
 
