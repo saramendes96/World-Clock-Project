475b7c9eac404dbf3d3582ece4c684828ed406b4
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-15 09:50:53 +0100

    Add link to navigate back to all cities

--- src/index.js
+++ src/index.js
@@ -55,6 +55,7 @@ function updateCity(event) {
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
   </div>
+  <a href="index.html">← Back to all cities</a>
 `}
 
 updateTime();
