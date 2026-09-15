910f467f25fb7a0359d0ba39fe4a708a9a6399fb
Sara Mendes <160604806+saramendes96@users.noreply.github.com>
2026-09-15 10:20:45 +0100

    Fix HTML escaping in city time display

--- src/index.js
+++ src/index.js
@@ -51,8 +51,10 @@ function updateCity(event) {
   let citiesElement = document.querySelector("#cities");
 
   citiesElement.innerHTML = `<div class="city" data-timezone="${cityTimeZone}">
+    <div>
     <h2>${cityName}</h2>
     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div>
   </div>
   <a href="index.html" class="home-link">← Back to all cities</a>
